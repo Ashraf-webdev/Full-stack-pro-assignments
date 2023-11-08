4098f9d9931bf720a1dae795aa1d13f13d6f589f
Ashraf-webdev <mdashrafullah742@gmail.com>
2023-11-08 10:47:49 +0530

    Solution of Javascript fundamental-02

---
+++ 18-Javascript_fundamental-02/Assignment-1/index.js
@@ -0,0 +1,8 @@
+
+let number = 12;
+
+// ternary operator to check if the number is even or odd
+const result = number % 2 === 0 ? "Even" : "Odd";
+
+// result
+console.log(`The number ${number} is ${result}.`);
---
+++ 18-Javascript_fundamental-02/Assignment-2/index.js
@@ -0,0 +1,28 @@
+
+// 1. Compact Variable Declaration and Assignment
+
+let a = 3, b = 6, c = a + b;
+
+// 2. Loop Control with Multiple Expressions
+
+for (let i = 0, j = 5; i < 3; i++, j--) {
+    console.log(`i: ${i}, j: ${j}`);
+  }
+
+// 3. In a while Loop
+
+let i = 0, sum = 0;
+while (i <= 10) sum += i, i++;
+
+
+// 4. Compact Array Initialization
+
+let arr = [1, 2, 3, 4, 5];
+
+// 5. Inline Object Initialization
+
+let person = { firstName: "John", lastName: "Doe", age: 30, country: "USA" };
+
+
+
+  
